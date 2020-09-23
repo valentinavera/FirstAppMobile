abf67ef47ced2d31762ee49fb89c4f3656e7b215
Valentina <valentinav@unicauca.edu.co>
2020-09-23 01:10:32 -0500

    layout test

--- app/src/main/java/edu/unicauca/principal/MainActivity.java
+++ app/src/main/java/edu/unicauca/principal/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.testprincipal);
         final Toast tag = Toast.makeText(this,"Ejecutando OnCreate()",Toast.LENGTH_SHORT);
         tag.show();
         TimeToast(tag, 10000);
