3a2a63f17b4b718d6b311f29139b01bd757d97d0
Valentina <valentinav@unicauca.edu.co>
2020-09-23 02:02:56 -0500

    funcion send message

--- app/src/main/java/edu/unicauca/principal/MainActivity.java
+++ app/src/main/java/edu/unicauca/principal/MainActivity.java
@@ -2,19 +2,30 @@ package edu.unicauca.principal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.testprincipal);
+        setContentView(R.layout.activity_main);
         final Toast tag = Toast.makeText(this,"Ejecutando OnCreate()",Toast.LENGTH_SHORT);
         tag.show();
         TimeToast(tag, 10000);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     @Override
